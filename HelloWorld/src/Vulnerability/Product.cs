35bce990808f8b98d3218bfe9fc74b779eeb563f
Chris Seiler <christopher.seiler@opswat.com>
2024-06-19 18:23:27 -0600

    Added initial project for Vulnerability Detection

---
+++ HelloWorld/src/Vulnerability/Product.cs
@@ -0,0 +1,17 @@
+﻿///////////////////////////////////////////////////////////////////////////////////////////////
+///  Sample Code for Acme Scanner
+///  Reference Implementation using OPSWAT MetaDefender Endpoint Security SDK
+///  
+///  Created by Chris Seiler
+///  OPSWAT OEM Solutions Architect
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace Compliance
+{
+    internal class Product
+    {
+        public int signatureId;
+        public string name;
+        public string vendor;
+    }
+}
