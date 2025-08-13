d6d718a5da10fc07070ca298af27c07440d410c0
ethan-may505 <ethan.may@opswat.com>
2025-08-13 11:48:59 -0400

    Windows OS Hello World - Vulnerabilities
    Created a HelloWorld to show windows OS vulenerabilities

--- HelloWorld/src/SDKDownloadLib/DownloadCatalog.cs
+++ HelloWorld/src/SDKDownloadLib/DownloadCatalog.cs
@@ -7,6 +7,7 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
 using SDKDownloader;
+using System;
 using System.IO;
 
 namespace SDKDownloadLib
@@ -32,6 +33,8 @@ namespace SDKDownloadLib
 
         public static string GetTokenDownloadURL(string fileName)
         {
+            Console.WriteLine("Can not find Token");
+            string DOWNLOAD_TOKEN = File.ReadAllText("download_token.txt");
             string result = VCR_URL.Replace("%token%", DOWNLOAD_TOKEN);
             result = result.Replace("%file%", fileName);
             return result;
